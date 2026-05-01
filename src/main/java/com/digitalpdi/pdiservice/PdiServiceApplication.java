01f1aa14750d322b3bf99711be1c6f79747b8ca8
zyneepylmaz <ylmazeynep01@gmail.com>
2026-05-01 12:07:45 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/digitalpdi/pdiservice/PdiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digitalpdi.pdiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalpdi/pdiservice/PdiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalpdi.pdiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
